a58c0abef7f36b148ae2ed83804cedd29e9f87d1
Marnix Klooster <marnix.klooster@gmail.com>
2005-10-03 07:05:13 +0000

    Updated demo0.mm from recent metamath tar ball

--- demo0.mm
+++ demo0.mm
@@ -4,7 +4,7 @@ $(
                       PUBLIC DOMAIN DEDICATION
 
 This file is placed in the public domain per the Creative Commons Public
-Domain Dedication.  http://creativecommons.org/licenses/publicdomain/
+Domain Dedication. http://creativecommons.org/licenses/publicdomain/
 
 Norman Megill - email: nm at alum.mit.edu
 
@@ -24,18 +24,22 @@ $( Specify properties of the metavariables $)
     ts $f term s $.
     wp $f wff P $.
     wq $f wff Q $.
-$( Define "term" and "wff" $)
+$( Define "term" (part 1) $)
     tze $a term 0 $.
+$( Define "term" (part 2) $)
     tpl $a term ( t + r ) $.
+$( Define "wff" (part 1) $)
     weq $a wff t = r $.
+$( Define "wff" (part 2) $)
     wim $a wff ( P -> Q ) $.
-$( State the axioms $)
+$( State axiom a1 $)
     a1 $a |- ( t = r -> ( t = s -> r = s ) ) $.
+$( State axiom a2 $)
     a2 $a |- ( t + 0 ) = t $.
-$( Define the modus ponens inference rule $)
     ${
        min $e |- P $.
        maj $e |- ( P -> Q ) $.
+$( Define the modus ponens inference rule $)
        mp  $a |- Q $.
     $}
 $( Prove a theorem $)
