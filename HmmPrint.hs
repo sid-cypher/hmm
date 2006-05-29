148d95aec327755d463e150a79bc6299b6b2d600
Marnix Klooster <marnix.klooster@gmail.com>
2006-05-29 09:36:34 +0000

    hmmprint: now taking label from command line

--- HmmPrint.hs
+++ HmmPrint.hs
@@ -2,10 +2,12 @@ module Main where
 
 import Hmm(mmParseFromString)
 import HmmPrinter(mmPrintProof)
+import System.Environment(getArgs)
 
 main :: IO ()
 main = do
 	dbText <- getContents
+	[label] <- getArgs
 	case mmParseFromString dbText of
 		Left err -> putStrLn ("Parse error: " ++ err)
-		Right (_, db) -> putStrLn $ mmPrintProof db "a1i" --TODO: get the label from the command line
+		Right (_, db) -> putStrLn $ mmPrintProof db label
